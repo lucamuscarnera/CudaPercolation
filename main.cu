46b1f04ff022a4e279308532a2ae35f24d0a3ac5
Luca Muscarnera <luca@DESKTOP-60SPNQS.>
2024-12-04 00:37:46 +0100

    structure of the folder

--- main.cu
+++ main.cu
@@ -1,6 +1,8 @@
 #include <iostream>
+#include "headers/test.h"
 
 int main()
 {
-    std::cout << "Ciao Mondo!" << std::endl;
+	std::cout << "Ciao Mondo!" << std::endl;
+	std::cout << test() << std::endl;
 }
