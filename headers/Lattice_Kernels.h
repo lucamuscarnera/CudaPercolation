d22a02280812d4d7c3234e3758b49d60aa9c2e8d
Luca Muscarnera <luca@DESKTOP-60SPNQS.>
2024-12-04 23:41:39 +0100

    The lattice observe kernel is created. If we constructed a memory region populated with the kernel_populate kernel, then E[ < 1(x < p) > ] = P[x < p] = p, by definition of uniform random variable. Thus this kernel applies the x -> 1( x < p ). The advantage of this approach, instead of sampling directly is that we have 'continuity' between the configurations, in a sense that can be formalized

--- headers/Lattice_Kernels.h
+++ headers/Lattice_Kernels.h
@@ -9,6 +9,9 @@
 #include <curand.h>
 #include <curand_kernel.h>
 
+
+// associates each site with the realization of a uniform random
+// variable between 0 and 1. 
 __global__ void lattice_populate(float * data, int N, int seed)
 {
 	// declare the state
@@ -23,6 +26,25 @@ __global__ void lattice_populate(float * data, int N, int seed)
 		// initialize the state
 		curand_init(seed, i, 0 , &state);
 		data[i] = curand_uniform(&state);
+	}
+}
+
+// Thresholds the value obtained through lattice_populate. The closer is p to 0, the lower is the threshold
+// to observe a "spin up"
+__global__ void lattice_observe(float * lattice_in,
+				unsigned char * lattice_out,
+				int N,
+				float p)
+{
+	// retrieve the index
+	int i = (threadIdx.x + blockIdx.x * blockDim.x) + N * (threadIdx.y + blockIdx.y * blockDim.y) ;
+
+	if(i < N)
+	{
+		if(lattice_in[i] > p)
+		{
+			lattice_out[i] = 1;
+		}
 	}
 }
 
